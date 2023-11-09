26a0e3e9ce454c1ef7a991857460014c95384113
iwasakitakehiro <iwasakitakehiro2@gmail.com>
2023-11-09 11:37:57 +0900

    tailwind使用のための修正

--- src/header.php
+++ src/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/output.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css">
     <title>NEW SITE</title>
 </head>

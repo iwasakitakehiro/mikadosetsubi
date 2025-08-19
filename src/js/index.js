4ab97b60189cfdd1f711321db0c0301215417ccb
iwasakitakehiro <iwasakitakehiro2@gmail.com>
2025-08-19 18:33:12 +0900

    20250819作業分

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,14 @@ import gsap from "gsap";
 const tl = gsap.timeline({ repeat: -1 });
 const images = gsap.utils.toArray(".mv-slide");
 const length = images.length;
-images.reverse();
+
+// 最初のスライドに初期クラスを設定
+images[0].classList.add("current-slide");
+images[1].classList.add("next-slide");
+images[length - 1].classList.add("prev-slide");
+
 images.forEach((image, index) => {
-  const count = index == length - 1 ? 0 : index + 1;
+  const count = index === length - 1 ? 0 : index + 1;
   tl.to(image, {
     maskImage: `linear-gradient(
       225deg,
@@ -17,35 +22,27 @@ images.forEach((image, index) => {
       transparent 0%,
       transparent 0%`,
     duration: 0.8,
-    delay: 5,
+    delay: 5, // 最初のスライドは遅延なし
     onStart: () => {
-      if (index == length - 1) {
-        images[index].classList.remove("next-slide");
-        images[0].classList.add("next-slide");
-        images[index].classList.add("current-slide");
-        images[index - 1].classList.remove("current-slide");
-      } else if (index == 0) {
-        images[index + 1].classList.add("next-slide");
-        images[index].classList.remove("next-slide");
-        images[index].classList.add("current-slide");
-        images[length - 1].classList.remove("current-slide");
-      } else {
-        images[index].classList.remove("next-slide");
-        images[index].classList.add("current-slide");
-        images[index + 1].classList.add("next-slide");
-        images[index - 1].classList.remove("current-slide");
-      }
-    },
-    onComplete: () => {
-      if (index == length - 1) {
-        images[index].classList.remove("current-slide");
-        images[0].classList.remove("next-slide");
-      }
+      // 現在のスライドをcurrent-slideに設定
+      images.forEach((img, i) => {
+        img.classList.remove("current-slide", "next-slide", "prev-slide");
+      });
+
+      image.classList.add("current-slide");
+
+      // 次のスライドをnext-slideに設定
+      const nextIndex = index === length - 1 ? 0 : index + 1;
+      images[nextIndex].classList.add("next-slide");
+
+      // 前のスライドをprev-slideに設定
+      const prevIndex = index === 0 ? length - 1 : index - 1;
+      images[prevIndex].classList.add("prev-slide");
     },
   }).to(
-    images[count],
+    ".next-slide img",
     {
-      scale: 1.1,
+      scale: 1.05,
       duration: 0.8,
     },
     "<"
